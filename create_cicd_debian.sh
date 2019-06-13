cb56017518dd31cb336d6b58266f55ae498803d4
root <root@debian.localdomain>
2019-06-13 17:14:42 +0200

    modified:   create_cicd_debian.sh

--- create_cicd_debian.sh
+++ create_cicd_debian.sh
@@ -100,7 +100,7 @@ kubectl taint nodes --all node-role.kubernetes.io/master-
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml
 $TOKEN=`kubectl -n kube-system describe secret default | grep "token:" | awk '{print $2}'`
 echo "kubernetes-dashboard token: ${TOKEN}"
-kubectl config set-credentials debian --token="${TOKEN}"
+kubectl config set-credentials kubernetes-dashboard --token="${TOKEN}"
 
 # Create namespace
 export NAMESPACE=cicd
