9673aca3340adb3c9f8083513c3087e8f80f73f6
root <root@debian.localdomain>
2019-06-13 17:08:02 +0200

    modified:   create_cicd_debian.sh

--- create_cicd_debian.sh
+++ create_cicd_debian.sh
@@ -97,7 +97,10 @@ kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
 # Dashboard UI installation
-kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy/recommended/kubernetes-dashboard.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml
+$TOKEN=`kubectl -n kube-system describe secret default | grep "token:" | awk '{print $2}'`
+echo "kubernetes-dashboard token: ${TOKEN}"
+kubectl config set-credentials debian --token="${TOKEN}"
 
 # Create namespace
 export NAMESPACE=cicd
@@ -178,38 +181,38 @@ done
 # helm install stable/nginx-ingress --tiller-namespace $NAMESPACE --namespace $NAMESPACE
 
 # Helm gitlab chart installation
-helm repo add gitlab https://charts.gitlab.io/
-helm repo update
-helm install gitlab/gitlab \
-  --name $NAMESPACE-gitlab \
-  --timeout 600 \
-  --set global.hosts.domain=mydomain.com \
-  --set global.hosts.externalIP=127.0.0.1 \
-  --set certmanager-issuer.email=gitlab@mydomain.com \
-  --kubeconfig config \
-  --tiller-namespace $NAMESPACE \
-  --namespace $NAMESPACE
-kubectl get secret $NAMESPACE-gitlab-initial-root-password -ojsonpath={.data.password} | base64 --decode ; echo
+#helm repo add gitlab https://charts.gitlab.io/
+#helm repo update
+#helm install gitlab/gitlab \
+#  --name $NAMESPACE-gitlab \
+#  --timeout 600 \
+#  --set global.hosts.domain=mydomain.com \
+#  --set global.hosts.externalIP=127.0.0.1 \
+#  --set certmanager-issuer.email=gitlab@mydomain.com \
+#  --kubeconfig config \
+#  --tiller-namespace $NAMESPACE \
+#  --namespace $NAMESPACE
+#kubectl get secret $NAMESPACE-gitlab-initial-root-password -ojsonpath={.data.password} | base64 --decode ; echo
 
 # Helm jupyterhub chart installation
-helm repo add jupyterhub https://jupyterhub.github.io/helm-chart
-helm repo update
-export HUB_COOKIE_SECRET=`openssl rand -hex 32`
-export PROXY_SECRET_TOKEN=`openssl rand -hex 32`
-cat - << EOF > config.yaml
-$NAMESPACE-hub:
-  cookieSecret: $HUB_COOKIE_SECRET
-$NAMESPACE-proxy:
-  secretToken: $PROXY_SECRET_TOKEN
-EOF
-helm install jupyterhub/jupyterhub \
-  --name $NAMESPACE-jupyterhub \
-  --timeout 600 \
-  --kubeconfig config \
-  --namespace $NAMESPACE \
-  --tiller-namespace $NAMESPACE \
-  --version 0.8.0 \
-  --values config.yaml
+#helm repo add jupyterhub https://jupyterhub.github.io/helm-chart
+#helm repo update
+#export HUB_COOKIE_SECRET=`openssl rand -hex 32`
+#export PROXY_SECRET_TOKEN=`openssl rand -hex 32`
+#cat - << EOF > config.yaml
+#$NAMESPACE-hub:
+#  cookieSecret: $HUB_COOKIE_SECRET
+#$NAMESPACE-proxy:
+#  secretToken: $PROXY_SECRET_TOKEN
+#EOF
+#helm install jupyterhub/jupyterhub \
+#  --name $NAMESPACE-jupyterhub \
+#  --timeout 600 \
+#  --kubeconfig config \
+#  --namespace $NAMESPACE \
+#  --tiller-namespace $NAMESPACE \
+#  --version 0.8.0 \
+#  --values config.yaml
 
 # Helm knative chart installation
 # helm install knative/knative --namespace $NAMESPACE
