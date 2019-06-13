777b7f4f0b2b3cd913f2f6940e25e9b8d80c9e41
root <root@debian.localdomain>
2019-06-13 19:19:06 +0200

    modified:   clean_deploys.sh

--- clean_deploys.sh
+++ clean_deploys.sh
@@ -9,5 +9,6 @@ helm del --purge gitlab-cicd --tiller-namespace $NAMESPACE
 kubectl delete rolebinding/tiller-clusterrolebinding -n $NAMESPACE
 kubectl delete deployment tiller-deploy -n $NAMESPACE
 kubectl delete serviceaccount tiller -n $NAMESPACE
+#kubectl delete all,secrets,sa,configmaps,deployments,ingresses,clusterroles,clusterrolebindings,virtualservices,destinationrules,customresourcedefinitions --selector=app=istio -n $NAMESPACE
 #kubeadm reset
 # rm -Rf /var/lib/etcd
