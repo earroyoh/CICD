394c51bd5a11f59fea8e71de72bb57c97a740912
earroyoh <enrique.arroyo@gmail.com>
2020-10-01 22:02:52 +0200

    modified:   start-istio.sh

--- start-istio.sh
+++ start-istio.sh
@@ -2,7 +2,7 @@
 # Debian/Ubuntu installation
 
 # Repo update
-#apt-get update -y
+#sudo apt-get update -y
 
 # CNI Calico installation
 # Wait for cluster to be in Ready, it can take a while
@@ -11,25 +11,22 @@
 #do
 #  sleep 5
 #done
-#kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
-#curl -L https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml -o calico.yaml
-#curl -L -o calico.yaml https://docs.projectcalico.org/v3.9/manifests/calico.yaml
 #POD_CIDR="172.30.0.0/16" \
 #	sed -i -e "s?192.168.0.0/16?$POD_CIDR?g" calico.yaml
-#kubectl apply -f calico.yaml
-#kubectl apply -f https://docs.projectcalico.org/v3.9/manifests/calico.yaml
+#kubectl apply -f https://docs.projectcalico.org/v3.16/manifests/calico.yaml
 
 # Comment out for a single node cluster to let it schedule pods
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
 # Create namespace
-export NAMESPACE=istio-system
-kubectl create namespace $NAMESPACE
+export KUBECONFIG=/home/debian/.kube/admin.conf
+kubectl apply -k github.com/istio/installer/base
+NAMESPACE=`kubectl get namespace | grep istio | awk '{print $1}'`
 
 # Helm installation
-curl -L https://storage.googleapis.com/kubernetes-helm/helm-v2.14.1-linux-amd64.tar.gz | tar zxvf -
-sudo mv linux-amd64/helm /usr/local/bin
-sudo mv linux-amd64/tiller /usr/local/bin
+#curl -L https://storage.googleapis.com/kubernetes-helm/helm-v2.14.1-linux-amd64.tar.gz | tar zxvf -
+#sudo mv linux-amd64/helm /usr/local/bin
+#sudo mv linux-amd64/tiller /usr/local/bin
 #curl -L https://bit.ly/install-helm | bash
 
 # Helm tiller service account creation
@@ -84,7 +81,15 @@ subjects:
 EOF
 kubectl create -f helm-clusterrolebinding.yaml
 
-helm init --service-account tiller --tiller-namespace $NAMESPACE
+# Tiller installation
+EXTERNAL_IP=`ip address show $IFACE | grep "inet " | awk '{print $2}' | awk -F'/' '{print $1}'`
+./gen_tiller_cert.sh $NAMESPACE $EXTERNAL_IP
+helm init --tiller-tls --tiller-tls-cert ./tiller.crt --tiller-tls-key ./tiller.key --tiller-tls-verify --tls-ca-cert /etc/kubernetes/pki/ca.crt --service-account tiller --tiller-namespace $TILLER_NAMESPACE
+cp helm.crt /home/debian/.helm/cert.pem
+cp helm.key /home/debian/.helm/key.pem
+cp ca.crt /home/debian/.helm/ca.pem
+sudo chown debian:debian $HOME/.helm/cert.pem $HOME/.helm/key.pem $HOME/.helm/ca.pem
+
 export TILLER_NAMESPACE=$NAMESPACE
 
 # Wait tiller to be in Running state, it can take a while
@@ -97,58 +102,15 @@ done
 # Create helm context config
 ./get_helm_token.sh $NAMESPACE
 
-# Helm nginx-ingress chart installation
+# Helm istio-ingress chart installation
 helm repo update
-helm install ingress-nginx/ingress-nginx --name $NAMESPACE --tiller-namespace $NAMESPACE --namespace $NAMESPACE --tls
-
-# Helm gitlab chart installation
-#helm repo add gitlab https://charts.gitlab.io/
-#helm repo update
-#helm install gitlab/gitlab \
-#  --name $NAMESPACE-gitlab \
-#  --timeout 600 \
-#  --set global.hosts.domain=mydomain.com \
-#  --set global.hosts.externalIP=127.0.0.1 \
-#  --set certmanager-issuer.email=gitlab@mydomain.com \
-#  --kubeconfig config \
-#  --tiller-namespace $NAMESPACE \
-#  --namespace $NAMESPACE
-#kubectl get secret $NAMESPACE-gitlab-initial-root-password -ojsonpath={.data.password} | base64 --decode ; echo
-
-# Helm jupyterhub chart installation
-#helm repo add jupyterhub https://jupyterhub.github.io/helm-chart
-#helm repo update
-#export HUB_COOKIE_SECRET=`openssl rand -hex 32`
-#export PROXY_SECRET_TOKEN=`openssl rand -hex 32`
-#cat - << EOF > config.yaml
-#$NAMESPACE-hub:
-#  cookieSecret: $HUB_COOKIE_SECRET
-#$NAMESPACE-proxy:
-#  secretToken: $PROXY_SECRET_TOKEN
-#EOF
-#helm install jupyterhub/jupyterhub \
-#  --name $NAMESPACE-jupyterhub \
-#  --timeout 600 \
-#  --kubeconfig config \
-#  --namespace $NAMESPACE \
-#  --tiller-namespace $NAMESPACE \
-#  --version 0.8.0 \
-#  --values config.yaml
-
-# Helm knative chart installation
-# helm install knative/knative --namespace $NAMESPACE
-
-# Helm jenkins chart installation
-# helm repo update
-# helm install stable/jenkins --name jenkins --namespace $NAMESPACE
-
-# Helm istio chart installation
-git clone https://github.com/istio/istio.git
-helm install istio/install/kubernetes/helm/istio-init --name istio-init --namespace $NAMESPACE --tls
+git clone https://github.com/istio/istio.git -b release-1.6.11-patch
+helm install --name istio-ingress istio/manifests/charts/gateways/istio-ingress -f istio/manifests/charts/global.yaml --tiller-namespace $NAMESPACE --namespace $NAMESPACE --tls
+helm install --name istio-discovery istio/manifests/charts/istio-control/istio-discovery -f istio/manifests/charts/global.yaml --tiller-namespace $NAMESPACE --namespace $NAMESPACE --tls
 
 # Wait istio-init to complete 
 echo "Waiting for istio-init to be in Running state..."
-while [ "`kubectl get -o template pod/$(kubectl get pods -n $NAMESPACE | grep istio-init | awk '{print $1}') -n $NAMESPACE --template={{.status.phase}}`" != "" ]
+while [ "`kubectl get -o template pod $(kubectl get pods -n $NAMESPACE | grep istio-init | awk '{print $1}') -n $NAMESPACE --template={{.status.phase}}`" != "" ]
 do
   sleep 5
 done
@@ -176,6 +138,6 @@ helm template \
     --set grafana.enabled=true \
     --set "kiali.dashboard.jaegerURL=http://jaeger-query:16686" \
     --set "kiali.dashboard.grafanaURL=http://grafana:3000" \
-    istio/install/kubernetes/helm/istio \
+    istio/manifests/charts/istiod-remote \
     --name istio --namespace $NAMESPACE > istio.yaml
 kubectl apply -f istio.yaml
